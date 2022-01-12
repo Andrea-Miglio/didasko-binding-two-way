8856aef3f29b19295958d2d9853903b996646496
Andrea-Miglio <migand85@gmail.com>
2022-01-12 15:19:07 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ViewChild, ElementRef, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +6,25 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  titolo:string = "Inserisci persona"
+  @ViewChild("rif_nome") private tagNome:ElementRef;
+  @ViewChild("rif_cognome") private tagCognome:ElementRef;
+  @ViewChild("rif_eta") private tagEta:ElementRef;
+
+  nome:string;
+  cognome:string;
+  eta:number;
+
+  inputValori():void{
+    this.nome = this.tagNome.nativeElement.value;
+    this.cognome = this.tagCognome.nativeElement.value;
+    this.eta = this.tagEta.nativeElement.value;
+  }
+
+  pulisciValori():void{
+    this.tagNome.nativeElement.value = "";
+    this.tagCognome.nativeElement.value = "";
+    this.tagEta.nativeElement.value = 0;
+    this.inputValori();
+  }
 }
